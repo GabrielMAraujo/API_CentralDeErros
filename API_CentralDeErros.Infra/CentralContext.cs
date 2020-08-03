44e9d40af9c77bc607b8ee100404e1e706ec7b70
Heron Rodrigues <heronrs1@gmail.com>
2020-08-03 19:46:08 -0300

    Alteração da conexão com o bd

--- API_CentralDeErros.Infra/CentralContext.cs
+++ API_CentralDeErros.Infra/CentralContext.cs
@@ -20,12 +20,10 @@ namespace API_CentralDeErros.Infra
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //Configurando o acesso ao servidor
-            if (!optionsBuilder.IsConfigured)
-                optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-VNQQ8BP\SQLEXPRESS;Initial Catalog=Codenation;Integrated Security=True");
-            //optionsBuilder.UseSqlServer($"Server={Configuration["env:DB_SERVER_ADDRESS"]};" +
-            //    $"Database={Configuration["env:DB_SERVER_DATABASE_NAME"]};" +
-            //    $"User Id={Configuration["env:DB_USER_ID"]};" +
-            //    $"Password={Configuration["env:DB_PASSWORD"]};");
+            optionsBuilder.UseSqlServer($"Server={Configuration["env:DB_SERVER_ADDRESS"]};" +
+                $"Database={Configuration["env:DB_SERVER_DATABASE_NAME"]};" +
+                $"User Id={Configuration["env:DB_USER_ID"]};" +
+                $"Password={Configuration["env:DB_PASSWORD"]};");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
