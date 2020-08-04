f28830be7b118f0395599c20148b5d226b0c220c
Gabriel Mota Araújo <gabriel.mota39@gmail.com>
2020-08-04 09:43:34 -0300

    Mais código da UserController

--- API_CentralDeErros.API/Controllers/UserController.cs
+++ API_CentralDeErros.API/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using API_CentralDeErros.Model;
 using API_CentralDeErros.Service.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,26 +11,41 @@ namespace API_CentralDeErros.API.Controllers
     {
         private readonly IUserService _service;
 
-        public UserController()
+        public UserController(IUserService service)
         {
+            _service = service;
         }
 
-        [HttpPost]
+        [HttpPost("/login")]
         public ActionResult<string> Login(string userName, string password)
         {
-            //Placeholder pro token no futuro
-            string token = "";
+            User user = _service.GetUser(userName, password);
 
-            return Ok(token);
+            //Se houver login no DB, retornar OK. Se não, retornar não autorizado
+            if (user != null)
+            {
+                return Ok(user.Token);
+            }
+            else
+            {
+                return StatusCode(401);
+            }
         }
 
-        [HttpPost]
-        public ActionResult<string> Register(string userName, string password)
+        [HttpPost("/register")]
+        public ActionResult<User> Register(string email, string userName, string password)
         {
-            //Placeholder pro token no futuro
-            string token = "";
+            User user = _service.AddUser(email, userName, password);
 
-            return Ok(token);
+            //Se foi possível inserir, retorna OK. 
+            if (user != null)
+            {
+                return user;
+            }
+            else
+            {
+                return StatusCode(500);
+            }
         }
     }
 }
